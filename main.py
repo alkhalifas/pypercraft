2765a66a8821f091867c97aec3fd9aa87e6d2d93
Saleh Alkhalifa <salehesam@gmail.com>
2023-07-31 20:57:28 -0400

    added driver

--- main.py
+++ main.py
@@ -2,7 +2,14 @@
 Main.py File to Start Applicaton
 """
 from pypercraft import pypercraft
+import os
+
 if __name__ == '__main__':
     print('PyCharm')
 
+    api_key = os.getenv("OPENAI_API_KEY")
+
+    paper = pypercraft.Pypercraft("A paper about starships from starwars", "space", 3, api_key).construct()
+
+    print(paper)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
