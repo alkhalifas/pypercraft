8779c4c388b09a03c12cfe5939bd41dc206455ce
alkhalifas <salehesam@gmail.com>
2023-08-01 22:04:26 -0400

    added download, not fully functional yet

--- ui.py
+++ ui.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import os
 from pypercraft.pypercraft import Pypercraft
+from docx import Document
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
 
 def main():
     st.title("Pypercraft Streamlit App")
@@ -13,10 +16,10 @@ def main():
     # Generate paper on button click
     if st.button("Generate Paper"):
         api_key = os.getenv("OPENAI_API_KEY")
-        py_crft = Pypercraft(query, topic, num_pages, api_key)
+        pypercraft = Pypercraft(query, topic, num_pages, api_key)
 
         with st.spinner("Generating Paper..."):
-            paper = py_crft.construct()
+            paper = pypercraft.construct()
 
         st.subheader("Title")
         st.write(paper["title"])
@@ -29,6 +32,35 @@ def main():
 
         st.subheader("Conclusion")
         st.write(paper["conclusion"])
+
+    # Export to DOCX and PDF on button click
+    if st.button("Export as DOCX and PDF"):
+        export_document(paper)
+
+
+def export_document(paper):
+    doc = Document()
+
+    # Adding title, introduction, body, and conclusion to the DOCX document
+    doc.add_heading(paper["title"], level=1)
+    doc.add_paragraph(paper["introduction"])
+    doc.add_paragraph(paper["body"])
+    doc.add_paragraph(paper["conclusion"])
+
+    # Save as DOCX
+    doc_filename = "generated_paper.docx"
+    doc.save(doc_filename)
+    st.success(f"DOCX file generated: {doc_filename}")
+
+    # Save as PDF
+    pdf_filename = "generated_paper.pdf"
+    c = canvas.Canvas(pdf_filename, pagesize=letter)
+    c.drawString(72, 800, paper["title"])
+    c.drawString(72, 750, paper["introduction"])
+    c.drawString(72, 700, paper["body"])
+    c.drawString(72, 650, paper["conclusion"])
+    c.save()
+    st.success(f"PDF file generated: {pdf_filename}")
 
 
 if __name__ == "__main__":
