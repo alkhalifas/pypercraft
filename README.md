07615c2787c9eddc6e2878ef1a9ad250324dff60
alkhalifas <salehesam@gmail.com>
2023-08-06 10:51:49 -0400

    added some tests and readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# pypercraft
+# Pypercraft
+#### A Simple Application of LLMs to Generate Documents
+
+## Live Demo
+
+A live demo of the Streamlit application can be seen here
+
+## Quick Start Guide
+
+    git clone git@github.com:alkhalifas/pypercraft.git
+
+    pip install -r requirements.txt
+
+    streamlit run ui.py
--- tests/test_pypercraft.py
+++ tests/test_pypercraft.py
@@ -2,6 +2,13 @@ import unittest
 from unittest.mock import patch
 from pypercraft.pypercraft import Pypercraft
 import os
+import datetime
+
+
+def get_current_datetime():
+    current_datetime = datetime.datetime.now()
+    return current_datetime
+
 
 class TestPypercraft(unittest.TestCase):
 
@@ -10,35 +17,34 @@ class TestPypercraft(unittest.TestCase):
         cls.api_key = os.getenv("OPENAI_API_KEY")
         cls.query = "An article about deep learning applications on space ships in star wars movie series"
         cls.topic = "Deep Learning and Star Wars"
-        cls.num_pages = 5
+        cls.num_pages = 2
         cls.pypercraft = Pypercraft(cls.query, cls.topic, cls.num_pages, cls.api_key)
 
     def test_generate_title(self):
         title = self.pypercraft.generate_title()
         self.assertIsInstance(title, str)
         self.assertNotEqual(title, "")
-        # print("title:", title)
+        self.assertGreater(len(title), 10)
 
     def test_generate_introduction(self):
         introduction = self.pypercraft.generate_introduction()
         self.assertIsInstance(introduction, str)
         self.assertNotEqual(introduction, "")
-        # print("introduction:", introduction)
+        self.assertGreater(len(introduction), 10)
 
     def test_generate_body(self):
         body = self.pypercraft.generate_body()
         self.assertIsInstance(body, str)
         self.assertNotEqual(body, "")
-        # print("body:", body)
+        self.assertGreater(len(body), 10)
 
     def test_generate_conclusion(self):
         conclusion = self.pypercraft.generate_conclusion()
         self.assertIsInstance(conclusion, str)
         self.assertNotEqual(conclusion, "")
-        # print("conclusion:", conclusion)
+        self.assertGreater(len(conclusion), 10)
 
     def test_construct(self):
-
         paper = self.pypercraft.construct()
 
         self.assertIsInstance(paper, dict)
@@ -50,9 +56,12 @@ class TestPypercraft(unittest.TestCase):
         self.assertNotEqual(paper["introduction"], "")
         self.assertNotEqual(paper["body"], "")
         self.assertNotEqual(paper["conclusion"], "")
+        self.assertGreater(len(paper["title"]), 10)
+        self.assertGreater(len(paper["introduction"]), 10)
+        self.assertGreater(len(paper["body"]), 10)
+        self.assertGreater(len(paper["conclusion"]), 10)
 
     def test_construct_export(self):
-
         paper = self.pypercraft.construct()
 
         self.assertIsInstance(paper, dict)
@@ -64,8 +73,11 @@ class TestPypercraft(unittest.TestCase):
         self.assertNotEqual(paper["introduction"], "")
         self.assertNotEqual(paper["body"], "")
         self.assertNotEqual(paper["conclusion"], "")
-        self.pypercraft.export_docx("tmp/test_file.docx")
-
+        self.assertGreater(len(paper["title"]), 10)
+        self.assertGreater(len(paper["introduction"]), 10)
+        self.assertGreater(len(paper["body"]), 10)
+        self.assertGreater(len(paper["conclusion"]), 10)
+        self.pypercraft.export_docx(f"tmp/test_file_{get_current_datetime()}.docx")
 
 
 if __name__ == "__main__":
