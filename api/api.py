971e149a45f742ae66203cb8127a5617de483d9c
alkhalifas <salehesam@gmail.com>
2023-08-06 21:39:10 -0400

    cleaning up code

--- api/api.py
+++ api/api.py
@@ -24,28 +24,39 @@ async def generate_document(
             ...,
             title="Number of Pages",
             description="The total number of documents to generate."),
+        tone: str = Query(
+            ...,
+            title="Tone",
+            description="The tone the writer should use"),
         api_key: str = Query(
             DEFAULT_API_KEY,
             title="API Key",
-            description="Your API key (optional)."),
+            description="Your API key."),
 ):
     """
     Function that generates a paper
     :param query: str
     :param topic: str
     :param num_pages: int
+    :param tone: str
     :param api_key: uuid
     :return:
     """
-    if not query or not topic or num_pages <= 0:
+    if not query or not topic or not tone or num_pages <= 0:
         raise HTTPException(
             status_code=400,
             detail="Invalid input. Please provide valid query, topic, and num_pages.")
+
+    if not api_key:
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid input. Please provide an API Key.")
 
     result = pypercraft.Pypercraft(
         query,
         topic,
         num_pages,
+        tone,
         DEFAULT_API_KEY
     ).construct()
 
