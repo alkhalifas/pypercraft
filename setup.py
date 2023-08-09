795ef4252c2a69c71abc84c8ce5f5fbe47568666
Saleh Alkhalifa <salehesam@gmail.com>
2023-08-08 20:25:56 -0400

    changed versions in install reqs

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ setup(
     author='alkhalifas',
     packages=find_packages(),
     install_requires=[
-        "fastapi == 0.100.1",
-        "langchain == 0.0.258",
-        "openai == 0.27.8",
-        "uvicorn == 0.23.2",
-        "pylint == 2.17.5",
-        "httpx == 0.24.1",
-        "streamlit == 1.25.0",
-        "python-docx == 0.8.11"
+        "fastapi >= 0.100.1",
+        "langchain >= 0.0.240",
+        "openai >= 0.27.8",
+        "uvicorn >= 0.23.2",
+        "pylint >= 2.17.5",
+        "httpx >= 0.24.1",
+        "streamlit >= 1.25.0",
+        "python-docx >= 0.8.11"
     ],
 )
