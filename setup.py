bf7227dad953e3de4310b95bd107191ace46367b
Saleh Alkhalifa <salehesam@gmail.com>
2023-08-08 20:13:42 -0400

    0.1.2 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pypercraft',
-    version='0.1.1',
+    version='0.1.2',
     description='Utilize Large Language Models to Craft Papers and Export them to Documents',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
